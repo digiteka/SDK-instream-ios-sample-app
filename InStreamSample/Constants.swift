ed60517ea48c9e28b42290b1d12b7ae69dceccbf
Armand Roussel <a.roussel@beapp.fr>
2024-01-10 13:46:42 +0100

    refacto : add configs in global constants class

---
+++ InStreamSample/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  InStreamSample
+//
+//  Created by ARoussel on 10/01/2024.
+//
+
+import Foundation
+
+class Constants {
+    static let mdtk = "01357940"
+    static let zone = "1"
+    static let src = "pqvp3r"
+    static let urlreferrer = ""
+    static let gdprconsentstring = "BOj8iv4Oj8iwYAHABAlxCS-AAAAnF7_______9______9uz_Ov_v_f__33e87_9v_l_7_-___u_-3zd4-_1vf99yfm1-7etr3tp_87ues2_Xur__59__3z3_9phPrsk89r633A/idfa/2D2859BC-7FB6-4225-A3E8-EED25341CA93"
+    static let tagparam = "video_app&sa=D&ust=1586938702508000&usg=AOvVaw0EoSE28fXl4HfVg-fQrA4n"
+}
--- InStreamSample/cell/VideoTableViewCell.swift
+++ InStreamSample/cell/VideoTableViewCell.swift
@@ -18,13 +18,8 @@ class VideoTableViewCell: UITableViewCell {
 
     func setUp(playMode: PlayMode) {
         if !isSetup {
-            let zone = "1"
-            let src = "pqvp3r"
-            let urlreferrer = ""
-            let gdprconsentstring = "BOj8iv4Oj8iwYAHABAlxCS-AAAAnF7_______9______9uz_Ov_v_f__33e87_9v_l_7_-___u_-3zd4-_1vf99yfm1-7etr3tp_87ues2_Xur__59__3z3_9phPrsk89r633A/idfa/2D2859BC-7FB6-4225-A3E8-EED25341CA93"
-            let tagparam = "video_app&sa=D&ust=1586938702508000&usg=AOvVaw0EoSE28fXl4HfVg-fQrA4n"
             do {
-                try videoView = InStream.shared.initMainPlayerWith(config: DTKISMainPlayerConfig(zone: zone, src: src, urlreferrer: urlreferrer, gdprconsentstring: gdprconsentstring, tagparam: tagparam, playMode: playMode))
+                try videoView = InStream.shared.initMainPlayerWith(config: DTKISMainPlayerConfig(zone: Constants.zone, src: Constants.src, urlreferrer: Constants.urlreferrer, gdprconsentstring: Constants.gdprconsentstring, tagparam: Constants.tagparam, playMode: playMode))
             }
             catch {
             }
